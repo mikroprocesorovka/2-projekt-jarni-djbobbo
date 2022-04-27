d5316a8282cd2bb4c857399e4a11c2f1f519542f
djbobbo <bohous.steier@seznam.cz>
2022-04-27 21:04:42 +0200

    Hotový projekt + dokumentace

--- src/main.c
+++ src/main.c
@@ -1,52 +1,73 @@
 #include "stm8s.h"
+
 #include "milis.h"
 
-//#include "delay.h"
-//#include <stdio.h>
-//#include "uart1.h"
+#include "stm8_hd44780.h"
+
+#include "delay.h"
+
+#include <stdio.h>
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
 
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+void encoder_init(void){
+    GPIO_Init(GPIOC, GPIO_PIN_1,GPIO_MODE_IN_PU_NO_IT);
+    GPIO_Init(GPIOC, GPIO_PIN_2,GPIO_MODE_IN_PU_NO_IT);
 
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
-
+    TIM1_DeInit();
+    TIM1_TimeBaseInit(16, TIM1_COUNTERMODE_UP, 500, 16); //inicializace enkoderu (void encoder_init)
+    TIM1_EncoderInterfaceConfig(TIM1_ENCODERMODE_TI12,TIM1_ICPOLARITY_FALLING,TIM1_ICPOLARITY_FALLING);
+   TIM1_Cmd(ENABLE);
+}
 
 void setup(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // taktovani MCU na 16MHz
 
-    init_milis();
-    //init_uart1();
+    lcd_init(); //inicializace LCD displeje
+    init_milis(); //inicializace milis
+
+    GPIO_Init(GPIOC,GPIO_PIN_5,GPIO_MODE_OUT_PP_LOW_SLOW); // PC5 output
+
+    encoder_init();
+
 }
-
 
 int main(void)
 {
-    uint32_t time = 0;
+    uint32_t time = milis(); //inicializace main
+
+    unsigned int present_value = 0x0000;
+    unsigned int previous_value = 0x0001;
+
+    char text[32];
+
+    uint32_t perioda;
 
     setup();
 
+    lcd_gotoxy(0,0);
+    lcd_puts("perioda");
     while (1) {
-
-        if (milis() - time > 333 && !BTN_PUSH) {
-            LED_REVERSE; 
+        if(milis() - time > perioda/2){
+            GPIO_WriteReverse(GPIOC,GPIO_PIN_5);
             time = milis();
-            //printf("%ld\n", time);
         }
 
-        //LED_REVERSE; 
-        //delay_ms(333);
-        //printf("Funguje to!!!\n");
+
+        present_value = TIM1_GetCounter();
+
+        if(present_value != previous_value)
+        {
+
+            lcd_gotoxy(12, 0);
+            sprintf(text,"%1u ",present_value*4);
+            lcd_puts(text);
+            perioda = present_value * 4;
+        }
+        previous_value = present_value;
+
     }
 }
 
